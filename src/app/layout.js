08d71f1c1875d6f65647c2a87c9a4b3cc2698c1a
Mayank Chaudhary <mr.singh244411@gmail.com>
2026-08-12 11:41:22 +0530

    Fix Google Analytics integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import Script from "next/script";
 import "./globals.css";
+
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import FloatingWhatsApp from "@/components/common/FloatingWhatsApp";
@@ -17,19 +19,29 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Header />
+
         {children}
+
         <Footer />
         <FloatingWhatsApp />
 
-        <script
-          async
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-DDCWP02K33"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-DDCWP02K33');
-        </script>
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+              window.dataLayer.push(arguments);
+            }
+
+            gtag("js", new Date());
+            gtag("config", "G-DDCWP02K33");
+          `}
+        </Script>
       </body>
     </html>
   );
