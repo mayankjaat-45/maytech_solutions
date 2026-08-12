a54ce3de6bae2cd8ecf05b71d3775426b4ae612e
Mayank Chaudhary <mr.singh244411@gmail.com>
2026-08-12 11:54:10 +0530

    google tag manager addedd

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,31 +17,45 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TLKVVXB5');
+          `}
+        </Script>
+      </head>
+
       <body>
+        {/* Google Tag Manager noscript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TLKVVXB5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+
         <Header />
 
         {children}
 
         <Footer />
         <FloatingWhatsApp />
-
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-DDCWP02K33"
-          strategy="afterInteractive"
-        />
-
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-
-            function gtag() {
-              window.dataLayer.push(arguments);
-            }
-
-            gtag("js", new Date());
-            gtag("config", "G-DDCWP02K33");
-          `}
-        </Script>
       </body>
     </html>
   );
